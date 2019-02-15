8c07c8c4f0dc4914bf3f016d36910500ccc92eaf
dettac <345098184@qq.com>
2019-02-15 12:09:45 +0800

    借阅图书修复

--- pages/core/jy/jy.js
+++ pages/core/jy/jy.js
@@ -28,32 +28,8 @@ Page({
       return false;
     }
     //判断并读取缓存
-    if(app.cache.jy){ jyRender(app.cache.jy); }
-    function jyRender(info){
-      info.nothing = !parseInt(info.books_num) || !info.book_list || !info.book_list.length;
-      var colors = ['green','yellow','red','purple'],
-          nowTime = new Date().getTime();
-      if(!info.nothing){
-        info.book_list.map(function(e){
-          var jDate = e.jsrq.split('-'), hDate = e.yhrq.split('-'),
-              jTime = new Date(jDate[0], jDate[1]-1, jDate[2]).getTime(),
-              hTime = new Date(hDate[0], hDate[1]-1, hDate[2]).getTime();
-          var sum = parseInt((hTime - jTime) / 1000 / 60 / 60 / 24),
-              timing = parseInt((hTime - nowTime) / 1000 / 60 / 60 / 24),
-              k = 1 - timing/sum, n = 0;
-          if(k < 0.3) { n = 0; }
-          else if(k < 0.7) { n = 1; }
-          else if(k <= 1) { n = 2; }
-          else if(k > 1) { n = 3; }
-          e.color = colors[n];
-          return e;
-        });
-      }
-      _this.setData({
-        jyData: info,
-        remind: ''
-      });
-    }
+    if(app.cache.jy){ _this.jyRender(app.cache.jy); }
+    
     wx.showNavigationBarLoading();
     wx.request({
       url: app._server + "/api/portal/books",
@@ -68,7 +44,11 @@ Page({
           if(info) {
             //保存借阅缓存
             app.saveCache('jy', info);
-            jyRender(info);          
+            _this.jyRender(info);
+            _this.setData({
+              jyHistoryTap: false,
+              remind: ''
+            });      
           }else{ _this.setData({ remind: '暂无数据' }); }
 
         }else{
@@ -91,18 +71,75 @@ Page({
       }
     });
   },
-  jyHistory: function(){
+  nowList:function(){
+    this.getData();
+  },
+  historyList: function(){
     var _this = this;
-    if(!_this.data.jyHistoryTap){
-      _this.setData({
-        jyHistoryTap: true
+    wx.showNavigationBarLoading();
+    wx.request({
+      url: app._server + "/api/portal/history_books",
+      method: 'POST',
+      data: app.key({
+        openid: app._user.openid,
+        id: app._user.we.id
+      }),
+      success: function (res) {
+        if (res.data && res.data.status === 200) {
+          var info = res.data.data;
+          console.log(info)
+          if (info) {
+            //保存借阅缓存
+            _this.jyRender(info);
+            _this.setData({
+              jyHistoryTap: true,
+              remind: ''
+            });
+          } else { _this.setData({ remind: '暂无数据' }); }
+
+        } else {
+          app.removeCache('jy');
+          _this.setData({
+            remind: res.data.message || '未知错误'
+          });
+        }
+      },
+      fail: function (res) {
+        if (_this.data.remind == '加载中') {
+          _this.setData({
+            remind: '网络错误'
+          });
+        }
+        console.warn('网络错误');
+      },
+      complete: function () {
+        wx.hideNavigationBarLoading();
+      }
+    });
+  },
+  jyRender:function(info){
+    var _this = this;
+    info.nothing = !parseInt(info.books_num) || !info.book_list || !info.book_list.length;
+    var colors = ['green', 'yellow', 'red', 'purple'],
+    nowTime = new Date().getTime();
+    if(!info.nothing) {
+      info.book_list.map(function (e) {
+        var jDate = e.jsrq.split('-'), hDate = e.yhrq.split('-'),
+          jTime = new Date(jDate[0], jDate[1] - 1, jDate[2]).getTime(),
+          hTime = new Date(hDate[0], hDate[1] - 1, hDate[2]).getTime();
+        var sum = parseInt((hTime - jTime) / 1000 / 60 / 60 / 24),
+          timing = parseInt((hTime - nowTime) / 1000 / 60 / 60 / 24),
+          k = 1 - timing / sum, n = 0;
+        if (k < 0.3) { n = 0; }
+        else if (k < 0.7) { n = 1; }
+        else if (k <= 1) { n = 2; }
+        else if (k > 1) { n = 3; }
+        e.color = colors[n];
+        return e;
       });
-      setTimeout(function(){
-        _this.setData({
-          jyHistoryTap: false
-        });
-      }, 2000);
     }
+    _this.setData({
+      jyData: info,
+    });
   }
- 
 });
